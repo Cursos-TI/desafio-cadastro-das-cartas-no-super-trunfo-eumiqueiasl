2d5bfc78e570cdba5d2535af632d0d62eae47b0d
eumiqueiasl <miqueiasneryleal@gmail.com>
2025-07-21 20:35:20 +0000

    iniciei as informações das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,16 @@
 int main(){
 
     printf("Desafio Super Trunfo - Países\n");
+    printf("A01\n");
 
+    int populacao;
+    float area;
+    float pib;
+    int Npontosturisticos;
+
+    printf("População:\n");
+    scanf("%d", populacao);
+    
 
 }
 
